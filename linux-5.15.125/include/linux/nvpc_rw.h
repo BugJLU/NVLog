73b575926c6ef4ff22ac64926d4273cd4d4201ae
Ownsky <wangguoyu7777@me.com>
2023-10-22 16:07:24 +0800

    feat(kernel): promote vec relevant functions refactor(kernel): split nvpc_rw from nvpc as seperate header and source files

---
+++ linux-5.15.125/include/linux/nvpc_rw.h
@@ -0,0 +1,23 @@
+#ifndef _LINUX_NVPC_RW_H
+#define _LINUX_NVPC_RW_H
+
+#include <linux/dax.h>
+#include <linux/uio.h>
+#include <linux/nvpc_base.h>
+
+/* copy data from kernel to nvpc */
+void nvpc_write_nv(void *from, loff_t off, size_t len);
+/* copy data from nvpc to kernel */
+void nvpc_read_nv(void *to, loff_t off, size_t len);
+
+/* copy data from user to nvpc */
+size_t nvpc_write_nv_iter(struct iov_iter *from, loff_t off, bool flush);
+// size_t nvpc_write_nv_iter_noflush(struct iov_iter *from, loff_t off, size_t len);
+/* copy data from nvpc to user */
+size_t nvpc_read_nv_iter(struct iov_iter *to, loff_t off);
+
+static inline void nvpc_wmb(void) {
+    pmem_wmb();
+}
+
+#endif
