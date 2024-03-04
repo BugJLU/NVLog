60f87e96f57e973593e8a7a96fa1719b34d2c39c
Jimmy Ho <jimmyhochina@outlook.com>
2024-03-04 08:35:12 +0800

    add: utils filebench-1.5-alpha3

--- linux-5.15.125/drivers/dax/nvpc.c
+++ linux-5.15.125/drivers/dax/nvpc.c
@@ -202,10 +202,10 @@ static int dev_dax_nvpc_probe(struct dev_dax *dev_dax)
 	
 	init_opts.dev = dev_dax->dax_dev;
 	init_opts.nid = numa_node;
-	init_opts.extend_lru = true; 		// NVTEST: Demote
-	init_opts.promote_level = 1; 		// NVTEST: Promote Level
-	init_opts.nvpc_lru_evict = true; 	// NVTEST: Evict
-	init_opts.absorb_syn = true; 		// NVTEST: SYN Subsystem
+	init_opts.extend_lru = false; 		// NVTEST: Demote
+	init_opts.promote_level = 0; 		// NVTEST: Promote Level
+	init_opts.nvpc_lru_evict = false; 	// NVTEST: Evict
+	init_opts.absorb_syn = false; 		// NVTEST: SYN Subsystem
 	init_opts.nvpc_sz = -1;		 		// NVTEST: NVPC NVM Size
 	init_opts.force = true;
 	init_opts.rebuild = true;
