60c81b4c865c1c2797adb6d153e397b985d4568b
Ownsky <wangguoyu7777@me.com>
2024-09-02 10:21:14 +0800

    plots update

--- utils/evaluation/plot6/draw/plot6.old.py
+++ utils/evaluation/plot6/draw/plot6.old.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fsnames = ['Ext-4', 'SPFS', 'NOVA', 'NVLog'] # P2CACHE
-clusters = ['Fillseq', 'Readseq', 'R.rand.w.rand'] # 'R.rand.w.rand\n(small)', 'R.rand.w.rand\n(large)'
+fsnames = ['Ext-4', 'SPFS', 'NOVA', 'NVPC'] # P2CACHE
+clusters = ['Fillseq', 'Readseq', 'R.rand.w.rand\n(small)', 'R.rand.w.rand\n(large)']
 
 data_s = {
     'Fillseq':                  [10952.66,	63857.33,	47443.66,	57283.33],
     'Readseq':                  [282103.66,	280035,	    233596,	    286742.33],
-    'R.rand.w.rand':      [118711.66,	164143,	    131666,	    163866.33],
-    # 'R.rand.w.rand\n(small)':      [118711.66,	164143,	    131666,	    163866.33],
-    # 'R.rand.w.rand\n(large)':      [37975.66,  46739.33,   88200,      98469]
+    'R.rand.w.rand\n(small)':      [118711.66,	164143,	    131666,	    163866.33],
+    'R.rand.w.rand\n(large)':      [37975.66,  46739.33,   88200,      98469]
 }
 data_l = {
     'Fillseq':                  [10952.66,	63857.33,	47443.66,	57283.33],
@@ -38,13 +37,13 @@ hatches = [
 
 for d in data:
     plt.rcParams['hatch.color'] = 'white'
-    plt.figure(figsize=(5, 2.5))
+    plt.figure(figsize=(6, 3))
     plt.ylabel("Throughput (Ops/s)")
-    plt.xticks(np.arange(3)*5+1.5, clusters)
+    plt.xticks(np.arange(4)*5+1.5, clusters)
     plt.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
     # plt.title(d)
     for i, clust in enumerate(clusters):
         h = plt.bar(np.arange(4)+5*i, data[d][clust], color=colors, hatch=hatches, label=fsnames)
     
     plt.legend(h, fsnames)
-    plt.savefig('rocksdb.pdf', bbox_inches='tight')
+    plt.savefig('plot6_'+d+'.pdf', bbox_inches='tight')
